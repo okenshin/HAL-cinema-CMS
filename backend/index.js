efb8a83a40a84687f56118f663c12b4cfef26597
Maro <maronomoto@gmail.com>
2023-11-16 18:40:00 +0900

    add

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,43 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2'); // mysql2ライブラリをインポート
 
 const app = express();
-const port = 4000; // サーバーポート
+const port = 4000;
 
-//Routers 設定
+// MySQL接続の設定
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'hal_cinema',
+};
+
+// MySQL接続プールを作成
+const pool = mysql.createPool(dbConfig);
+
+// MySQLプールをルーターからアクセス可能にする
+app.use((req, res, next) => {
+  req.mysql = pool;
+  next();
+});
+
+// ルーターの設定
 const movieRouter = require('./routers/movieRoute');
 const cmsRouter = require('./routers/cmsRoute');
 const scheduleRouter = require('./routers/scheduleRoute');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/movie',movieRouter);
-app.use('/cms',cmsRouter);
-app.use('/schedule',scheduleRouter);
+app.use('/movie', movieRouter);
+app.use('/cms', cmsRouter);
+app.use('/schedule', scheduleRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello World');
 });
 
 app.listen(port, () => {
--- backend/routers/movieRoute.js
+++ backend/routers/movieRoute.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',function(req,res,next) {
-    res.json({
-        id : 'hp1',
-        title : 'ハリーポッターと賢者の石',
-        context : 'テキスト入力',
-        length : '01:20:23',
-    });
+router.get('/', (req, res) => {
+  // MySQLプールにアクセス
+  const pool = req.mysql;
+
+  // MySQLクエリや操作を実行
+
+
+  //ここから欲しいデータの取得するクエリを書いていく
+  pool.query('SELECT * FROM movie', (error, results, fields) => {
+    if (error) throw error;
+    res.json(results);
+  });
 });
 
 module.exports = router;
