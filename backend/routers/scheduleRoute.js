265630155897f9142314e3628ddb80779eaa4f2a
Maro <maronomoto@gmail.com>
2023-11-16 19:19:01 +0900

    add comment

--- backend/routers/scheduleRoute.js
+++ backend/routers/scheduleRoute.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
 
   // MySQLクエリや操作を実行
 
-  //本日のスケジュール情報
+  //本日のスケジュール情報取得
   const query = 'SELECT s.scheduleID, m.movieName, s.screenID, s.scheduleStartDatetime FROM schedule s JOIN movie m ON s.movieID = m.movieID WHERE DATE(s.scheduleStartDatetime) = CURRENT_DATE() ORDER BY s.scheduleStartDatetime';
 
   pool.query(query, (error, results, fields) => {
