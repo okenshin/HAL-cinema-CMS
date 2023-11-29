62b6085846500154fdbfddcdb86485bc09cdfa2c
HasegawaTomohiko <T.Hasegawa0603@gmail.com>
2023-11-29 14:35:24 +0900

    cms追加処理

--- backend/routers/cmsRoute.js
+++ backend/routers/cmsRoute.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.json({
-        id: 'abcdefg',
-        name: '林 信明',
-        cmsRole: 'admin'
+router.get('/', function(req, res) {
+    const pool = req.mysql;
+    pool.query('SELECT * FROM cms', (error, results, fields) => {
+        if(error){
+            console.error(error);
+        }
+        res.json(results);
+    });
+});
+
+router.post('/', function(req,res) {
+    const pool = req.mysql;
+    const cmsId = req.body.cmsId;
+    const cmsPass = req.body.cmsPass;
+    const cmsRole = req.body.cmsRole;
+
+    const query = 'INSERT INTO cms (cmsID, cmsPass, cmsRole) VALUES (?,?,?)';
+
+    pool.query(query,[cmsId,cmsPass,cmsRole], (error,results,fields) => {
+        if (error) {
+            console.error(error);
+        }
+        res.json({ msg : '新規追加しました！'});
     });
 });
 
