96d95d23e71c5beeba7c15258b06464cba92c686
Maro <maronomoto@gmail.com>
2023-11-30 17:19:43 +0900

    ログイン、新規登録処理実装

--- backend/routers/cmsRoute.js
+++ backend/routers/cmsRoute.js
@@ -1,29 +1,81 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 router.get('/', function(req, res) {
     const pool = req.mysql;
     pool.query('SELECT * FROM cms', (error, results, fields) => {
-        if(error){
+        if (error) {
             console.error(error);
+            return res.status(500).json({ error: 'サーバーエラー' });
         }
         res.json(results);
     });
 });
 
-router.post('/', function(req,res) {
+
+router.post('/', function(req, res) {
     const pool = req.mysql;
     const cmsId = req.body.cmsId;
     const cmsPass = req.body.cmsPass;
     const cmsRole = req.body.cmsRole;
 
-    const query = 'INSERT INTO cms (cmsID, cmsPass, cmsRole) VALUES (?,?,?)';
+    // パスワードのハッシュ化
+    try {
+        const hashedPassword = bcrypt.hashSync(cmsPass, 10);
+        
+        const query = 'INSERT INTO cms (cmsID, cmsPass, cmsRole) VALUES (?,?,?)';
 
-    pool.query(query,[cmsId,cmsPass,cmsRole], (error,results,fields) => {
+        pool.query(query, [cmsId, hashedPassword, cmsRole], (error, results, fields) => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({ error: 'サーバーエラー' });
+            }
+            res.json({ msg: '新規追加しました！' });
+        });
+    } catch (hashError) {
+        console.error(hashError);
+        return res.status(500).json({ error: 'ハッシュ化エラー' });
+    }
+});
+
+router.post('/login', function(req, res) {
+    const pool = req.mysql;
+    const cmsId = req.body.cmsId;
+    const cmsPass = req.body.cmsPass;
+
+    const query = 'SELECT * FROM cms WHERE cmsID = ?';
+
+    pool.query(query, [cmsId], (error, results, fields) => {
         if (error) {
             console.error(error);
+            return res.status(500).json({ error: 'サーバーエラー' });
         }
-        res.json({ msg : '新規追加しました！'});
+
+        // ID重複確認
+        if (results.length === 0) {
+            return res.status(401).json({ error: '無効な情報' });
+        }
+
+        const user = results[0];
+
+        // ハッシュ化パスワードの比較
+        bcrypt.compare(cmsPass, user.cmsPass, (bcryptError, bcryptResult) => {
+            if (bcryptError) {
+                console.error(bcryptError);
+                return res.status(500).json({ error: 'サーバーエラー' });
+            }
+
+            if (!bcryptResult) {
+                return res.status(401).json({ error: '無効な情報' });
+            }
+
+            // セッションに保存
+            req.session.cmsId = user.cmsID;
+            req.session.cmsRole = user.cmsRole;
+
+            res.json({ msg: 'ログイン成功', user: user });
+        });
     });
 });
 
