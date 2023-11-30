52b822aa6eef71e697053dcc22683d4e1fbcd0d3
okenshin <akiless-2002-@softbank.ne.jp>
2023-11-30 15:55:57 +0900

    あ

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,11 +1,19 @@
 import Link from "next/link";
+import { useState } from "react";
 
-export default function index(){
+export default function index(props){
+
+
+
   return (
     <>
       <h1>Hello world!!!</h1>
       <Link href="/next">
         next
+      </Link>
+
+      <Link href="/movie">
+        movie
       </Link>
     </>
   );
