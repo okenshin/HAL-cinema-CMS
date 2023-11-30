630fc6e77130c641378211b06d36a75ab9315397
rannawaybaby <myuto1479@gmail.com>
2023-11-30 18:05:58 +0900

    add movie Routes

--- backend/routers/movieRoute.js
+++ backend/routers/movieRoute.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const uuid = require('uuid');
+const bcrypt = require('bcrypt'); // 必要ならばbcryptを追加
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, '/Applications/XAMPP/xamppfiles/htdocs/halcinema2/HALcinema2/image/movie');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.originalname.split('.').pop();
+    const filename = uuid.v4() + '.' + ext;
+    cb(null, filename);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 router.get('/', (req, res) => {
   // MySQLプールにアクセス
@@ -12,6 +28,28 @@ router.get('/', (req, res) => {
   pool.query('SELECT * FROM movie', (error, results, fields) => {
     if (error) throw error;
     res.json(results);
+  });
+});
+
+// 画像を保存するエンドポイント
+router.post('/', upload.fields([{ name : 'movieImage', maxCount : 1}]), (req, res) => {
+  const pool = req.mysql;
+  const movieID = req.body.movieID;
+  const movieContext = req.body.movieContext;
+  const movieName = req.body.movieName;
+  //const moviemovieImagePath = req.body.movieImagePath;
+  const movieImageName = req.files.movieImage[0].filename;
+  const movieLenght = req.body.movieLenght;
+
+  const query = 'INSERT INTO movie (movieID, movieName, movieContext, movieImagePath ,movieLenght) VALUES (?,?,?,?,?)';
+
+  pool.query(query, [movieID,movieName,movieContext,movieImageName,movieLenght], (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.json({ msg: '新規追加しました！' });
+    }
   });
 });
 
